e1c9ba7cae55e3895c269023d2ab4bed08a28844
EDUARDOMBRAGA <eduardomendoncabraga@gmail.com>
2014-11-16 18:18:01 +0000

    Outra etapa: views-urls-index.html

--- controle/views.py
+++ controle/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from controle.forms import PatrimonioForm
+from controle.models import Controle
+
+def index(request):
+    form = PatrimonioForm()
+    return render(request, 'index.html', {'form': form})
+
+def validar(request):
+	if request.method == 'POST':
+		form = PatrimonioForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+	
+			controle = Controle.objects.all().order_by('nome')
+
+			return render(request,'validar.html',{'form':form,'controle':controle})
+		else:
+		    return render(request,'index.html',{'form':form})
 
 # Create your views here.
