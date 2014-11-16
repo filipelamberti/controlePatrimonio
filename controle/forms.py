97c13a3238944594cfe7f57ccddf091c3ca5ea87
EDUARDOMBRAGA <eduardomendoncabraga@gmail.com>
2014-11-16 17:52:09 +0000

    Model e forms 0.0

--- controle/forms.py
+++ controle/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from controle.models import Controle
 
 class PatrimonioForm(forms.Form):
-    nome = forms.CharField(max_length='50', required=True)
-    descricao = forms.CharField(max_length='100', required=True)
-    valor = forms.CharField(max_length='10', required=True)
-    status = forms.CharField(max_length='1', blank=True)
+    class Meta:
+        model = Controle
+        
     
